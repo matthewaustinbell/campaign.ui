76120fa8a650e854e5a664d0bfbc6d0dcc543680
unknown <matthewaustinbell@gmail.com>
2020-02-11 17:40:34 -0600

    wip

--- src/data/questionData.js
+++ src/data/questionData.js
@@ -5,6 +5,7 @@ const baseUrl = 'https://localhost:44322/api/opentriviadb';
 const getOneQuestion = () => new Promise((resolve, reject) => {
     axios.get(`${baseUrl}`)
         .then((result) => {
+            //questions
             const questionResults = result.data;
             questionResults.question = decodeURI(questionResults.question);
             questionResults.question = questionResults.question.replace(/&quot;/g, '\ ');
@@ -15,8 +16,7 @@ const getOneQuestion = () => new Promise((resolve, reject) => {
             questionResults.question = questionResults.question.replace(/&hellip;/g, '\ '); 
             questionResults.question = questionResults.question.replace(/&rdquo;/g, '\ '); 
             //answers
-           //questionResults.answer.option1 = questionResults.answer.option1.replace(/&quot;/g, '\ ');
-            // questionResults.answer = questionResults.answer.replace(/&quot;/g, '\ ');
+            //questionResults.answer = questionResults.answer.replace(/&quot;/g, '\ ');
             // questionResults.answer = questionResults.answer.replace(/&#039;/g, '\ '); 
             // questionResults.answer = questionResults.answer.replace(/&rsquo;/g, '\ '); 
             // questionResults.answer = questionResults.answer.replace(/&ldquo;/g, '\ '); 
