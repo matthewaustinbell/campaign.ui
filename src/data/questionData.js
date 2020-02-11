d955ae346cb9d8ea82eac8861f3c991bcafb0627
unknown <matthewaustinbell@gmail.com>
2020-02-11 11:11:10 -0600

    character bug for question resolved, needs to be applied to answers

--- src/data/questionData.js
+++ src/data/questionData.js
@@ -6,8 +6,24 @@ const getOneQuestion = () => new Promise((resolve, reject) => {
     axios.get(`${baseUrl}`)
         .then((result) => {
             const questionResults = result.data;
-            // questionResults.question = questionResults.question.replace(/&quot;/g, '\\"');
             questionResults.question = decodeURI(questionResults.question);
+            questionResults.question = questionResults.question.replace(/&quot;/g, '\ ');
+            questionResults.question = questionResults.question.replace(/&quot;/g, '\ ');
+            questionResults.question = questionResults.question.replace(/&#039;/g, '\ '); 
+            questionResults.question = questionResults.question.replace(/&rsquo;/g, '\ '); 
+            questionResults.question = questionResults.question.replace(/&ldquo;/g, '\ '); 
+            questionResults.question = questionResults.question.replace(/&hellip;/g, '\ '); 
+            questionResults.question = questionResults.question.replace(/&rdquo;/g, '\ '); 
+            //answers
+           //questionResults.answer.option1 = questionResults.answer.option1.replace(/&quot;/g, '\ ');
+            // questionResults.answer = questionResults.answer.replace(/&quot;/g, '\ ');
+            // questionResults.answer = questionResults.answer.replace(/&#039;/g, '\ '); 
+            // questionResults.answer = questionResults.answer.replace(/&rsquo;/g, '\ '); 
+            // questionResults.answer = questionResults.answer.replace(/&ldquo;/g, '\ '); 
+            // questionResults.answer = questionResults.answer.replace(/&hellip;/g, '\ '); 
+            // questionResults.answer = questionResults.answer.replace(/&rdquo;/g, '\ '); 
+
+
             resolve(questionResults);
     })
         .catch(err => reject(reject));
