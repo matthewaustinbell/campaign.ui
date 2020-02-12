7098194bb78f26162a47d3a142b45a313dc0ac7b
unknown <matthewaustinbell@gmail.com>
2020-02-11 19:52:14 -0600

    fixed correct answer ascii code

--- src/data/questionData.js
+++ src/data/questionData.js
@@ -5,7 +5,8 @@ const baseUrl = 'https://localhost:44322/api/opentriviadb';
 const getOneQuestion = () => new Promise((resolve, reject) => {
     axios.get(`${baseUrl}`)
         .then((result) => {
-            //questions
+
+            //Questions
             const questionResults = result.data;
             questionResults.question = decodeURI(questionResults.question);
             questionResults.question = questionResults.question.replace(/&quot;/g, '\ ');
@@ -15,16 +16,31 @@ const getOneQuestion = () => new Promise((resolve, reject) => {
             questionResults.question = questionResults.question.replace(/&ldquo;/g, '\ '); 
             questionResults.question = questionResults.question.replace(/&hellip;/g, '\ '); 
             questionResults.question = questionResults.question.replace(/&rdquo;/g, '\ '); 
-            //answers
-            //questionResults.answer = questionResults.answer.replace(/&quot;/g, '\ ');
-            // questionResults.answer = questionResults.answer.replace(/&#039;/g, '\ '); 
-            // questionResults.answer = questionResults.answer.replace(/&rsquo;/g, '\ '); 
-            // questionResults.answer = questionResults.answer.replace(/&ldquo;/g, '\ '); 
-            // questionResults.answer = questionResults.answer.replace(/&hellip;/g, '\ '); 
-            // questionResults.answer = questionResults.answer.replace(/&rdquo;/g, '\ '); 
 
+            //Correctanswers
+            questionResults.correct_answer = decodeURI(questionResults.correct_answer);
+            questionResults.correct_answer = questionResults.correct_answer.replace(/&quot;/g, '\ ');
+            questionResults.correct_answer = questionResults.correct_answer.replace(/&#039;/g, '\ ');
+            questionResults.correct_answer = questionResults.correct_answer.replace(/&rsquo;/g, '\ ');
+            questionResults.correct_answer = questionResults.correct_answer.replace(/&ldquo;/g, '\ ');
+            questionResults.correct_answer = questionResults.correct_answer.replace(/&helipp;/g, '\ ');
+            questionResults.correct_answer = questionResults.correct_answer.replace(/rdquo;/g, '\ ');
 
-            resolve(questionResults);
+            //IncorrectAnswers
+            //  questionResults.incorrect_answers = decodeURI(questionResults.incorrect_answers);
+            //  questionResults.incorrect_answers = quesationResult.incorrect_answers.map(singleIncorrectAnswers => {
+            //      singleIncorrectAnswers.replace( 
+                     
+            //     } 
+            //    questionResults.incorrect_answers.replace(/&#039;/g, '\ ');
+            //     questionResults.incorrect_answers.replace(/&quot;/g, '\ ');
+            //     questionResults.incorrect_answers.replace(/&rsquo;/g, '\ ');
+            //     questionResults.incorrect_answers.replace(/&ldquo;/g, '\ ');
+            //     questionResults.incorrect_answers.replace(/&helipp;/g, '\ ');
+            //     questionResults.incorrect_answers.replace(/&rdquo;/g, '\ ');
+        
+            
+                resolve(questionResults);
     })
         .catch(err => reject(reject));
 });
