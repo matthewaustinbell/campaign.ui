2edf7bcc3e917551ce0d8f7f006449cfb60a35b3
unknown <matthewaustinbell@gmail.com>
2020-03-06 09:31:28 -0600

    wip

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -4,8 +4,8 @@ const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-    <span className="counter-score">{props.score}</span>
-            <button className="counter-action decrement"> + </button>
+                <span className="counter-score">{props.score}</span>
+            <button className="counter-action increment"> + </button>
         </div>
     );
 }
