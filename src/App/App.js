e621ce78268f66669afc1f993596bb1ec235e726
unknown <matthewaustinbell@gmail.com>
2020-09-11 15:54:46 -0500

    removing notes for clean code

--- src/App/App.js
+++ src/App/App.js
@@ -38,7 +38,6 @@ class App extends React.Component {
     }
 
   handleClick = () => {
-    // getMyQuestion();
     QuestionData.getOneQuestion()
     .then(response => this.setState({question : response})) 
     .then(()=>{
